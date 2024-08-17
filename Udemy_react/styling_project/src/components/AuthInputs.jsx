adf26c16f65d3a003efb54f8f0340fa2b08fc7d5
Tomás Brogueira <tomas.brogueira@tecnico.ulisboa.pt>
2024-08-17 12:18:51 +0100

    authinputs

--- Udemy_react/styling_project/src/components/AuthInputs.jsx
+++ Udemy_react/styling_project/src/components/AuthInputs.jsx
@@ -27,6 +27,9 @@ export default function AuthInputs() {
           <label>Email</label>
           <input
             type="email"
+            style={{
+              backgroundColor : emailNotValid ? '9a3412' : 'd1d5db'
+            }}
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
